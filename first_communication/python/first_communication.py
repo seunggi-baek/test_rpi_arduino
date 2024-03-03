68be314b2572235d575d4294a1a2f7a75650efd6
seunggi-baek <back2back2@naver.com>
2024-03-03 09:53:26 +0900

    first_communication

---
+++ first_communication/python/first_communication.py
@@ -0,0 +1,9 @@
+#/usr/bin env python3
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
+time.sleep(3)
+ser.reset_input_buffer()
+print("Serial OK")
+ser.close()
