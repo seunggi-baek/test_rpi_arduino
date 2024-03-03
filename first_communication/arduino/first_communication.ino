fa3bca94a406da603d6cc9c2f57b3766176641b6
seunggi-baek <back2back2@naver.com>
2024-03-03 13:18:20 +0900

    Send data from Arduino to Raspberry Pi

--- first_communication/arduino/first_communication.ino
+++ first_communication/arduino/first_communication.ino
@@ -1,11 +1,10 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   while(!Serial) {}
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Hello from Arduino");
+  delay(1000);
 }
--- first_communication/python/first_communication.py
+++ first_communication/python/first_communication.py
@@ -4,6 +4,18 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
 time.sleep(3)
+
+if(ser.isOpen() == False):
+    ser.open()
+
 ser.reset_input_buffer()
 print("Serial OK")
-ser.close()
+
+try:
+    while True:
+        time.sleep(0.01)
+        line = ser.readline().decode('utf-8')
+        print(f"rpi3 : {line}")
+except KeyboardInterrupt:
+    print("Close Serial")
+    ser.close()
